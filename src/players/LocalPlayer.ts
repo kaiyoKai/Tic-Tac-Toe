bf2dd9e6f853a29b04836005da114846e4662c93
kaiyoKai <kheiarikai0@gmail.com>
2026-02-13 00:09:25 +0100

    added some cleanup logic for the subscribers of the localplayer object to prevent possible memory leaks

--- src/players/LocalPlayer.ts
+++ src/players/LocalPlayer.ts
@@ -1,6 +1,6 @@
 import type { Position } from "../types/Common.js";
 import type { Player } from "./Player.js";
-import EventBus from "../services/EventBus.js";
+import EventBus, { type Subscription } from "../services/EventBus.js";
 import type { GameEventMap } from "../types/Events.ts";
 export class LocalPlayer implements Player {
   constructor(
@@ -10,10 +10,24 @@ export class LocalPlayer implements Player {
     private eventBus: EventBus<GameEventMap>,
   ) {}
 
-  async makeMove(): Promise<Position> {
+  async makeMove(): Promise<Position | null> {
     return new Promise((resolve) => {
-      this.eventBus.on("ui:cell-clicked", (data) => {
+      let clickSub: Subscription;
+      let resetSub: Subscription;
+
+      const cleanup = () => {
+        clickSub.unsubscribe();
+        resetSub.unsubscribe();
+      };
+
+      clickSub = this.eventBus.on("ui:cell-clicked", (data) => {
+        cleanup();
         resolve(data);
+      });
+
+      resetSub = this.eventBus.on("ui:reset-requested", () => {
+        cleanup();
+        resolve(null);
       });
     });
   }
